c60a015bb58f7d81c57436bf4013894cc4bc30ac
Styrkar <stykki@hotmail.com>
2017-02-19 17:05:09 +0000

    Hellow world MVC
    the "Model" and "Controller"

---
+++ MVC/Controller.php
@@ -0,0 +1,14 @@
+<?php
+class Controller {
+    private $model;
+
+    public function __construct(Model $model) {
+        $this->model = $model;
+    }
+
+    public function textClicked() {
+        $this->model->text = 'Text Updated';
+    }
+}
+
+?> 
